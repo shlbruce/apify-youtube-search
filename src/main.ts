f4e2001fef9a33b97152ff3e07612cbc229438ba
shlbruce <shlbruce@hotmail.com>
2025-07-05 16:05:12 -0500

    increase screens size works. should use this line  instead of     const page = await browser.newPage();
    const page = await context.newPage();

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,20 @@ type VideoResult = { title: string; url: string };
 type Input = { keywords: string[] };
 
 
+const width = 2048;
+const height = 1152;
+
 async function main() {
     await Actor.init();
     const input = await Actor.getInput() as Input;
     const keywords: string[] = input?.keywords || [];
 
     //const browser = await chromium.launch({ headless: true });
-    const browser = await chromium.launch({ headless: false, slowMo: 100});
-    const page = await browser.newPage();
+    const browser = await chromium.launch({ headless: false, slowMo: 100, args: [`--window-size=${width},${height}`] });
+    const context = await browser.newContext({
+        viewport: { width, height }
+    });
+    const page = await context.newPage();
 
     const maxCount = 50; // <--- Change this to your preferred number
 
